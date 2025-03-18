6458da2c7d90925427b38de055524d664d7ef98e
João <joaogianoni.tds2.senai@gmail.com>
2025-03-18 09:54:13 -0300

    feat: create card component

--- src/app/titulos/page.jsx
+++ src/app/titulos/page.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from "./titulos.module.css";
 import Header from "../components/header";
+import Footer from "../components/footer";
+import Card from "../components/cards";
 
-const CorinthiansTrophiesPage = () => {
+export default function Titulos() {
   return (
     <div className={styles.container}>
       <Header />
@@ -10,20 +12,9 @@ const CorinthiansTrophiesPage = () => {
         <section className={styles.featured}>
           <h2>Conquistas Principais</h2>
           <div className={styles.featuredTrophies}>
-            <div className={styles.trophy}>
-              <div className={styles.trophyCover}>
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"
-                  alt="Troféu do Mundial de Clubes da FIFA 2012"
-                />
-              </div>
-              <div className={styles.trophyInfo}>
-                <h3>Mundial de Clubes da FIFA</h3>
-                <p className={styles.year}>2012</p>
-                <p className={styles.details}>Corinthians 1 x 0 Chelsea</p>
-                <button className={styles.detailsButton}>Ver Detalhes</button>
-              </div>
-            </div>
+            <Card ano={2012} capa={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/FIFA_CLUB_WORLDCUP.jpg/450px-FIFA_CLUB_WORLDCUP.jpg"}
+          detalhes={"Corinthians 1 x 0 Chelsea"} textoAlt={"Troféu do mundial interclubes"} titulo={"Mundial de clubes da FIFA"} />
+            
             <div className={styles.trophy}>
               <div className={styles.trophyCover}>
                 <img
@@ -213,10 +204,7 @@ const CorinthiansTrophiesPage = () => {
           </div>
         </section>
       </main>
-
-
+      <Footer />
     </div>
   );
 };
-
-export default CorinthiansTrophiesPage;
